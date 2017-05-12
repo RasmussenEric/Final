6fb53f630a3082e58bed333a2f041527ec47acf9
RasmussenEric <ericrasmussen17@yahoo.com>
2017-05-12 11:26:09 -0700

    FinalProject

---
+++ Files/FinalServer.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class FinalServer extends JFrame
+{
+	
+	private JTextField userInput;
+	private JTextArea chatArea;
+	private ObjectOutputStream output;
+	private ObjectInputStream input;
+	private ServerSocket Server;
+	private Socket connection;
+	//private String message;
+	
+	
+	//create text box, setsize and setvisible
+	//listen for actions
+	public FinalServer()
+	{
+	
+
+	
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ FinalTutorial/Test.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Test
+{
+	public static void main(String[] args)
+	{
+		Server sally = new Server();
+		sally.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		sally.startRunning();
+		
+		
+		
+	}
+}
